8923887abf1b4948ee17dac36b0f2e42fdc4b360
krisb <krisanathep.b@gli.co.th>
2020-04-06 08:07:47 +0700

    First Deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
+import 'leaflet/dist/leaflet.css';
+import MapView from './components/MapView';
+import './Css/App.scss';
+import Axios from 'axios';
+import ListView from './components/ListView';
+import DetailsView from './components/DetailsView';
+
+const api = 'https://coronavirus-tracker-api.herokuapp.com/v2/locations';
 
 function App() {
+  const [locationArr, setLocationArr] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [mapCenter, setMapCenter] = useState([13, 100]);
+
+  function sortedLocationArray(locations) {
+    return [...locations].sort((location1, location2) => {
+      return location2.latest.confirmed - location1.latest.confirmed;
+    });
+  }
+
+  const onSelectLocation = useCallback(
+    id => {
+      const location = locationArr.find(location => location.id === id);
+      if (!location) {
+        setSelectedLocation(null);
+        return;
+      }
+      const {
+        coordinates: { latitude, longitude },
+      } = location;
+      setMapCenter([latitude, longitude]);
+      setSelectedLocation(location);
+    },
+    [locationArr],
+  );
+
+  const onDeselectLocation = useCallback(() => {
+    setSelectedLocation(null);
+  }, []);
+
+  useEffect(() => {
+    Axios.get(api)
+      .then(response => {
+        const sortedLocations = sortedLocationArray(response.data.locations);
+        setLocationArr(sortedLocations);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  let detailsView = null;
+  if (!!selectedLocation) {
+    detailsView = (
+      <DetailsView
+        location={selectedLocation}
+        onClickClose={onDeselectLocation}
+      />
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ListView
+        locationArr={locationArr}
+        selectedLocation={selectedLocation}
+        onSelectItem={onSelectLocation}
+        onDeselectItem={onDeselectLocation}
+      />
+      <MapView onSelectMarker={onSelectLocation} locationArr={locationArr} mapCenter={mapCenter} />
+      {detailsView}
     </div>
   );
 }
